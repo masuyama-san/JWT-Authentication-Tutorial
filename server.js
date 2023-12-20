e650b01d21c1228f2cb068856e61bdb43e762863
masuyama-san <soichi_masuyama@narutech.co.jp>
2023-12-20 09:13:00 +0900

    12/20 9:12

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
 const jwt = require('jsonwebtoken')
 
-app.user(express.json())
+app.use(express.json())
 
 const posts = [
     {
@@ -16,16 +18,29 @@ const posts = [
     }
 ]
 
-app.get('/posts', (req, res) =>{
-    res.json(posts)
+app.get('/posts', authenticateToken, (req, res) =>{
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
-app.post('/login', (req, res) =>{
-    //ユーザー認証
-    const username = req.body.username
-    const user = {name: username}
+// app.post('/login', (req, res) =>{
+//     //ユーザー認証
+//     const username = req.body.username
+//     const user = {name: username}
 
-    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-})
+//     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+//     res.json({accessToken: accessToken})
+// })
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null)return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) =>{
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000)
